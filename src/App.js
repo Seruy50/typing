63666e93134cb2789ad17ebae3c3f0dd282d73ae
Seruy50 <Seruy50@ukr.net>
2023-01-30 19:53:11 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import heart from './1.jpg'
+import heartNo from './2.jpg'
 
 function App() {
 
@@ -11,15 +13,22 @@ const [copyStop, setCopyStop] = useState(false);
 const [bgc, setBgc] = useState({
   background: 'white'
 })
-const [line, setLine] = useState('30%')
-
+const [line, setLine] = useState('30%');
+const [life, setLife] = useState([<span className="heartSpan" key="0"><img src={heart} alt="none" /></span>,
+  <span className="heartSpan" key="1"><img src={heart} alt="none" /></span>, 
+    <span className="heartSpan" key="2"><img src={heart} alt="none" /></span>]);
+const [buttonAllow, setButtonAllow] = useState(false)
 
 
 
 //let str = 'У 1908 році було прокладено першу залізницю, що поєднала Володимир-Волинський із Ковелем на півночі і далі з Києвом, діяли поштово-телеграфна контора, вісім готелів, два кінотеатри, чотири фабрично-заводські підприємства.  '
-let str = 'Слово'
+let str = 'Слово';
 
-
+const lifeEnd = () => {
+  let lifes = life;
+  lifes.pop();
+  setLife(lifes);
+}
 
 let changeText = (e) => {
   setAreaText(e.target.value);
@@ -36,6 +45,7 @@ let changeText = (e) => {
     clearInterval(intervalID);
   } else if(count <= 0) {
     setProblem(true); 
+    lifeEnd();
   }
   
 }
@@ -71,46 +81,57 @@ const timerReset = () => {
   timer();
 }
 
+
+
 let newAttempt = () => {
-  setProblem(false);
-  setAreaText('');
-  timerReset();
-  setResult('')
-  setLine(0)
+  if(life.length === 0){
+    console.log('itsOver');
+    setButtonAllow(true);
+  } else {
+    setProblem(false);
+    setAreaText('');
+    timerReset();
+    setResult('');
+    setLine(0);
+  }
 }
 
 
 
 return <>
-  <div className='pseudoHeader'>
-    <div className='text' onCopy={() => setCopyStop(true)}>{str}</div>
-  </div>
-  <div className='centralPart'>
-    <div className='buttons'>
-      <div className='forButton'>
-        <button>Reset</button>
+    <div className='pseudoHeader'>
+      <div className='text' onCopy={() => setCopyStop(true)}>{str}</div>
+    </div>
+    <div className='centralPart'>
+      <div className='buttons'>
+        <div className='forButton'>
+          <button>Reset</button>
+        </div>
+        <div className='forButton'>
+        <button className='forButtonTry' onClick={newAttempt} disabled={buttonAllow}>Спробувати</button>
+        </div>
       </div>
-      <div className='forButton'>
-      <button className='forButtonTry' onClick={newAttempt}>Спробувати</button>
+      <div>
+      <textarea value={areaText} style={bgc} id="textArea"  disabled={problem} onChange={changeText} onPaste={() => setCopyStop(true)}></textarea>
+      </div>
+      <div className='count'>
+      <p>{count <= 0 ? '' : count}</p>
       </div>
     </div>
     <div>
-    <textarea value={areaText} style={bgc} id="textArea"  disabled={problem} onChange={changeText} onPaste={() => setCopyStop(true)}></textarea>
-    </div>
-    <div className='count'>
-    <p>{count <= 0 ? '' : count}</p>
-    </div>
-  </div>
-  <div>
-    <div className="line">
-      <div className="progres" style={{width: line + '%'}}>
+      <div className="line">
+        <div className="progres" style={{width: line + '%'}}>
+        </div>
       </div>
     </div>
-  </div>
-  <div>
-    <p className='mistake'>{copyStop ? 'Дружок, а ти не охринів часом?' : ''}</p>
-    <p className="result">{result}</p>
-  </div>
+    <div className="hearts">
+      {life}
+    </div>
+    <div>
+      <p className='mistake'>{copyStop ? 'Дружок, а ти не охринів часом?' : ''}</p>
+      <p className="result">{result}</p>
+      <p className="mistake">{buttonAllow ? 'Перепрошую, проте ви викристали усі спроби. Для того, щоб спробувати знову, натисніть на кнопку reset': ''}</p>
+    </div>
  </>
 }
 
